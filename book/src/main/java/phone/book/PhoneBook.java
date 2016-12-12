244097004bc9438d9419e8c3767e2a1ad4a747e4
ArtyrG <artyr_galeev@mail.ru>
2016-12-12 23:23:56 +0300

    Input data, shows names and numbers.

--- book/src/main/java/phone/book/PhoneBook.java
+++ book/src/main/java/phone/book/PhoneBook.java
@@ -1,5 +1,8 @@
 package phone.book;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,7 +13,7 @@ import java.util.Map;
  */
 public class PhoneBook 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
     	// create ArrayList list, list1 is empty, list2 for save number phone
     	ArrayList<String> list = new ArrayList<String>(); 
@@ -27,6 +30,21 @@ public class PhoneBook
     	map.put("Ivanov I.I.", list1); 
     	map.put("Petrov B.G.", list2); 
     	map.put("Hollp W.V.", list1); 
-
-    }
+    	
+    	// reader for reading data
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String name = reader.readLine();
+		
+		// When you are in the map shows the name and phone no name
+		if (map.containsKey(name)) {
+			if (map.get(name) == list1) {
+				System.out.println("Не найдено ФИО в БД");
+			} else {
+				System.out.println("Номер(а) телефона(ов) :");
+				System.out.println(map.get(name));
+			}
+		} else {
+			System.out.println("Не найдено ФИО в БД");
+		}
+	}
 }
